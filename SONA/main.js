7fa53ca1facf2046ca117b4c7c00680f1d02ee39
hmbgaq1 <hmbgaq1@gmail.com>
2022-08-11 18:26:55 +0900

    220811

--- SONA/main.js
+++ SONA/main.js
@@ -6,6 +6,11 @@ const allMenu = document.querySelector('nav .allMenu');
 
 const left = document.querySelector('main .main_slide .left');
 const right = document.querySelector('main .main_slide .right');
+
+const main_slide = document.querySelector('main .main_slide');
+const imgs = document.querySelectorAll('main .main_slide .imgs li');
+let currentIndex = 0;
+
 const moveToTopBtn = document.querySelector('main .product_box .rightquick .moveToTopBtn');
 const moveToBottomBtn = document.querySelector('main .product_box .rightquick .moveToBottomBtn');
 
@@ -20,6 +25,22 @@ buttonAll.addEventListener('click', (e) => {
     left.classList.toggle('active');
     right.classList.toggle('active');
 });
+
+
+left.addEventListener('click', (e) => {
+    currentIndex--;
+    // currentIndex = currentIndex < 0 ? 0 : currentIndex
+    main_slide.style.marginLeft = `${-1600*(currentIndex)}px`
+    console.log('left', currentIndex)
+});
+
+right.addEventListener('click', (e) => {
+    currentIndex++;
+    // currentIndex = currentIndex >= imgs.length ? imgs.length-1 : currentIndex
+    main_slide.style.marginLeft = `${-1600*(currentIndex)}px`
+    console.log('right', currentIndex)
+});
+
 
 moveToTopBtn.addEventListener('click', (e) => {
     window.scrollTo({ top: 0, behavior: "smooth" });  
